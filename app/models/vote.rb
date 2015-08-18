f7e5dce654e1888e59e674e68f831d881f60ba2b
RicciFlowing <benjamin@mathe-sellin.de>
2015-08-18 17:27:56 +0200

    Changed the rendering of the VoteUp Button, now it is only rendered if there is no vote from the current_user. Otherwise it will render a simple text 'Allready voted'. Added  a self.has_voted(user, idea) to the Vote model.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,12 @@ class Vote < ActiveRecord::Base
   validates :idea, uniqueness: {scope: :user}
   belongs_to :user
   belongs_to :idea
+
+  def self.has_voted?(user,idea)
+    if self.where(user: user, idea: idea).count > 0
+      return true
+    else
+      return false
+    end
+  end
 end
